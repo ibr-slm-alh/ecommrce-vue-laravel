cfe610aae3107feda124e1adaad8743f1346ad27
ibr-slm-alh <a.b.r2018-1998@hotmail.com>
2023-02-05 12:28:53 +0200

    message

---
+++ backend/src/components/AppLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="min-h-full bg-gray-200 flex">
+        <Sidebar :class="{'-ml-[200px]':!sidebarOpened}"/>
+        <div class="flex-1">
+            <Navbar @toggle-sidebar="toggleSidebar"></Navbar>
+            <main class="p-6">
+                <router-view></router-view>
+            </main>
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidebar from "./Sidebar.vue";
+export default {
+    name: "AppLayout",
+    components: {Sidebar}
+}
+</script>
+
+<style scoped>
+
+</style>
--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,14 +1,31 @@
 import {createRouter,createWebHistory} from 'vue-router';
+import AppLayout from "../components/AppLayout.vue";
 import Dashboard from '../views/Dashboard.vue';
 import Login from "../views/Login.vue";
 import RequestPassword from "../views/requestpassword.vue";
 import ResetPassword from "../views/resetpassword.vue";
+import store from "../store/index.js";
 
 const routes = [
     {
-        path:'/dashboard',
-        name:'dashboard',
-        component: Dashboard
+      path: '/app',
+      name: 'app',
+      component: AppLayout,
+      meta:{
+        requiresAuth: true
+      },
+      children:[
+        {
+            path:'dashboard',
+            name:'app.dashboard',
+            component: Dashboard
+        },
+        {
+            path:'products',
+            name:'app.products',
+            component: Products
+        },
+      ]
     },
     {
         path:'/login',
@@ -30,6 +47,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),   //domain.com#/users
     routes
+})
+
+router.beforeEach((to,from,next)=>{
+    if(to.meta.requiresAuth && store.state.user.token){
+        next({name:'login'})
+    }else if(to.meta.requiresGuest && store.state.token){
+        next({name:'app.dashboard'})
+    }else{
+        next();
+    }
 })
 
 export default router
